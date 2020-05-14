335445bd7a2dc01d00d51d2187b0b82bf32bed49
Rug <noah.price.237@gmail.com>
2020-05-14 15:03:27 -0400

    Revert "Revert "Added Axis swap, Added StringProperty panel""
    This reverts commit 5644b5aa845232af90ff9e25849cd2b8ffb7d461.

--- BatchFBXExporter.py
+++ BatchFBXExporter.py
@@ -1,67 +1,80 @@
-bl_info = {
-    # Blender Add-On data
-    "name": "FBX Batch Exporter",
-    "blender": (2, 80, 75),
-    "category": "Object",
-
-}
-
-
 import bpy
 import os
 
-class FBXBatchExporter(bpy.types.Operator):
-    # Blender operator Data
-    """FBX Batch Exporter"""
-    bl_idname = "object.fbx_batch_exporter"
-    bl_label = "FBX Batch Exporter"
-    bl_options = {'REGISTER', 'UNDO'}
-    
-    
-    # Operator's funciton
-    def execute(self, context):
-    
-    #Get context for operations
-    
-        # export to blend file location
-        basedir = os.path.dirname(bpy.data.filepath)    # Change this to be a user defined path
+#----------------------------------
+# Properties for our exporter panel
+#----------------------------------
 
-        view_layer = bpy.context.view_layer
+class PathProperties(PropertyGroup):
+    path : StringProperty(
+        name="Export Directory Path",
+        description="Path to export directory",
+        default="",
+        maxlen=1024,
+        subtype='DIR_PATH')
 
-        obj_active = view_layer.objects.active
-        selection = bpy.context.selected_objects
+# Swap coordinates on all selected objects
+def swap_axis():
+    selection = bpy.context.selected_objects
 
-        bpy.ops.object.select_all(action='DESELECT')
+    for obj in selection:
+        
+        bpy.ops.transform.rotate(value=-1.5708, orient_axis='X', orient_type='GLOBAL', constraint_axis=(True, False, False))    # Rotate -90d in radians
+        bpy.ops.object.transform_apply(location=False, rotation=True, scale=False)                                              # Apply rotation
+        bpy.ops.transform.rotate(value=1.5708, orient_axis='X', orient_type='GLOBAL', constraint_axis=(True, False, False))     # Rotate 90d in radians
 
-        for obj in selection:
+# export to blend file location
+def export_fbx(self, context):
+    basedir = os.path.dirname(bpy.data.filepath)    # Change this to be a user defined path
+    view_layer = bpy.context.view_layer
 
-            obj.select_set(True)
+    obj_active = view_layer.objects.active
+    selection = bpy.context.selected_objects
 
-            name = bpy.path.clean_name(obj.name) # Remove any funky characters
-            fn = os.path.join(basedir, name) # Decide file name, path
-            
-            # Export in .FBX, only Meshes, apply scale using FBX Scale Units
-            bpy.ops.export_scene.fbx(filepath=fn + ".fbx", use_selection=True, object_types={'MESH'}, apply_scale_options='FBX_SCALE_UNITS')
+    bpy.ops.object.select_all(action='DESELECT')
 
-            obj.select_set(False)
+    for obj in selection:
 
-            print("written:", fn) # Print what we created
+        obj.select_set(True)
+
+        name = bpy.path.clean_name(obj.name) # Remove any funky characters
+        fn = os.path.join(basedir, name) # Decide file name, path
+        
+        # Export in .FBX, only Meshes, apply scale using FBX Scale Units
+        bpy.ops.export_scene.fbx(filepath=fn + ".fbx", use_selection=True, object_types={'MESH'}, apply_scale_options='FBX_SCALE_UNITS')
+
+        obj.select_set(False)
+
+        print("written:", fn) # Print what we created
 
 
-        view_layer.objects.active = obj_active
+    view_layer.objects.active = obj_active
 
-        for obj in selection:
-            obj.select_set(True)
-            
-        return {'FINISHED'}
-# Register this operator inside Blender's internal list when this add-on is enabled
+    for obj in selection:
+        obj.select_set(True)
+        
+classes = (
+    PathProperties
+)
+
 def register():
-    bpy.utils.register_class(FBXBatchExporter)
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+        
+    bpy.types.Scene.fbx_exporter = PointerProperty(type=PathProperties)
     
-# Unregister this operator inside Blender's internal list when this add-on is disabled
-def unregister():
-    bpy.utils.unregister_class(FBXBatchExporter)
-    
-if __name__ == "__main__":
+    def unregister():
+        from bpy.utils import unregister_class
+        for cls in reversed(classes):
+            unregister_class(cls)
+        del bpy.types.Scene.fbx_exporter
+        
+if __name_ == "__main__":
     register()
 
+
+
+
+
+    
